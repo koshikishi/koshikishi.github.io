7925364b0e45710914f623468f8f81fa284dd8b7
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2023-07-19 12:29:26 +0300

    Updated gulpfile and project config files

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -17,10 +17,29 @@ import {create as bsCreate} from 'browser-sync';
 const {src, dest, watch, series, parallel} = gulp;
 const browserSync = bsCreate();
 
-// Compiling of *.css files from *.scss with autoprefixer and minification
+// Paths to files
+const Path = {
+  Source: {
+    ROOT: 'source',
+    STYLES: 'source/styles',
+    JS: 'source/js',
+    IMAGES: 'source/images',
+    ICONS: 'source/images/icons',
+    FAVICONS: 'source/images/favicons',
+    FONTS: 'source/fonts',
+  },
+  Build: {
+    ROOT: 'build',
+    STYLES: 'build/css',
+    JS: 'build/js',
+    IMAGES: 'build/images',
+  },
+};
+
+// Compiling *.css files from *.scss with autoprefixer and minification
 const sass = gulpSass(dartSass);
 
-export const styles = () => src('source/styles/style.scss')
+export const styles = () => src(`${Path.Source.STYLES}/style.scss`)
   .pipe(plumber())
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
@@ -32,41 +51,41 @@ export const styles = () => src('source/styles/style.scss')
     suffix: '.min',
   }))
   .pipe(sourcemaps.write('.'))
-  .pipe(dest('build/css'))
+  .pipe(dest(Path.Build.STYLES))
   .pipe(browserSync.stream());
 
 // Minification of *.html files
-export const html = () => src('source/*.html')
+export const html = () => src(`${Path.Source.ROOT}/*.html`)
   .pipe(htmlmin({
     collapseWhitespace: true,
     minifyCSS: true,
     minifyJS: true,
   }))
-  .pipe(dest('build'));
+  .pipe(dest(Path.Build.ROOT));
 
 // Minification of *.js script files
-export const scripts = () => src('source/js/*.js')
+export const scripts = () => src(`${Path.Source.JS}/*.js`)
   .pipe(sourcemaps.init())
   .pipe(terser())
   .pipe(rename({
     suffix: '.min',
   }))
   .pipe(sourcemaps.write('.'))
-  .pipe(dest('build/js'));
+  .pipe(dest(Path.Build.JS));
 
-// Compression of raster image files with generation of *.webp format
+// Compressing raster image files with generation of *.webp format
 export const optimizeImages = () => src([
-  'source/images/**/*.{png,jpg}',
-  '!source/images/favicons/**',
+  `${Path.Source.IMAGES}/**/*.{png,jpg}`,
+  `!${Path.Source.FAVICONS}/**`,
 ])
   .pipe(sharp({
     webp: {
-      quality: 90,
+      quality: 80,
       effort: 6,
     },
   }))
-  .pipe(dest('build/images'))
-  .pipe(src('source/images/**/*.{png,jpg}'))
+  .pipe(dest(Path.Build.IMAGES))
+  .pipe(src(`${Path.Source.IMAGES}/**/*.{png,jpg}`))
   .pipe(sharp({
     png_to_png: {
       compressionLevel: 9,
@@ -78,50 +97,50 @@ export const optimizeImages = () => src([
       mozjpeg: true,
     },
   }))
-  .pipe(dest('build/images'));
+  .pipe(dest(Path.Build.IMAGES));
 
-// Compression of vector image *.svg files
+// Compressing vector image *.svg files
 export const optimizeSvg = () => src([
-  'source/images/**/*.svg',
-  '!source/images/icon-*.svg',
+  `${Path.Source.IMAGES}/**/*.svg`,
+  `!${Path.Source.ICONS}/**`,
 ])
   .pipe(svgmin({
     multipass: true,
   }))
-  .pipe(dest('build/images'));
+  .pipe(dest(Path.Build.IMAGES));
 
 // Copying image files
 export const copyImages = () => src([
-  'source/images/**/*.{png,jpg,svg}',
-  '!source/images/icon-*.svg',
+  `${Path.Source.IMAGES}/**/*.{png,jpg,svg}`,
+  `!${Path.Source.ICONS}/**`,
 ])
-  .pipe(dest('build/images'));
+  .pipe(dest(Path.Build.IMAGES));
 
 // Fast generation of image files in *.webp format
 export const fastWebp = () => src([
-  'source/images/**/*.{png,jpg}',
-  '!source/images/favicons/**',
+  `${Path.Source.IMAGES}/**/*.{png,jpg}`,
+  `!${Path.Source.FAVICONS}/**`,
 ])
   .pipe(sharp({
     webp: {
       effort: 0,
     },
   }))
-  .pipe(dest('build/images'));
+  .pipe(dest(Path.Build.IMAGES));
 
 // Deleting files in the build directory before copying
-export const clean = () => deleteAsync('build');
+export const clean = () => deleteAsync(Path.Build.ROOT);
 
 // Copying files to the build directory
 export const copy = (done) => {
   src([
-    'source/fonts/**/*.{woff,woff2}',
-    'source/*.ico',
-    'source/*.webmanifest',
+    `${Path.Source.FONTS}/**/*.{woff,woff2}`,
+    `${Path.Source.ROOT}/*.ico`,
+    `${Path.Source.ROOT}/*.webmanifest`,
   ], {
-    base: 'source',
+    base: Path.Source.ROOT,
   })
-    .pipe(dest('build'));
+    .pipe(dest(Path.Build.ROOT));
   done();
 };
 
@@ -131,11 +150,11 @@ export const refresh = (done) => {
   done();
 };
 
-// Starting Browsersync server
+// Start Browsersync server
 export const server = (done) => {
   browserSync.init({
     ui: false,
-    server: 'build',
+    server: Path.Build.ROOT,
     cors: true,
     notify: false,
   });
@@ -144,12 +163,12 @@ export const server = (done) => {
 
 // Watching changes in project files
 export const watcher = () => {
-  watch('source/styles/**/*.scss', styles);
-  watch('source/js/*.js', scripts);
-  watch('source/*.html', series(html, refresh));
+  watch(`${Path.Source.STYLES}/**/*.scss`, styles);
+  watch(`${Path.Source.JS}/**/*.js`, scripts);
+  watch(`${Path.Source.ROOT}/*.html`, series(html, refresh));
 };
 
-// Build the project
+// Build the project for production
 export const build = series(
   clean,
   parallel(
